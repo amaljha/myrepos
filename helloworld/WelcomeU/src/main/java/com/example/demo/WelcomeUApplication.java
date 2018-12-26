9165810028fe9a0311a53dc0d3cf06239c960141
amaljha <35734845+amaljha@users.noreply.github.com>
2018-12-26 15:18:05 +0530

    Add files via upload

---
+++ helloworld/WelcomeU/src/main/java/com/example/demo/WelcomeUApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeUApplication.class, args);
+	}
+}
